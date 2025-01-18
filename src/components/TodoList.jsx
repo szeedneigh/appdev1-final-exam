e65cee05d24e216f1581db54b946547cb9e30e6f
szeedneigh <sidneyjohn1704@gmail.com>
2025-01-18 13:37:36 +0800

    Add TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import { useTodos } from "../context/TodoContext";
+
+const TodoList = () => {
+  const { todos, loading, toggleComplete, deleteTodo, addTodo } = useTodos();
+  const [newTodo, setNewTodo] = useState("");
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <TodoForm
+        addTodo={addTodo}
+        newTodo={newTodo}
+        setNewTodo={setNewTodo}
+      />
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{ textDecoration: todo.completed ? "line-through" : "none", cursor: "pointer" }}
+              onClick={() => toggleComplete(todo.id)}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
